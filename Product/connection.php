698c55b7eb8901b0cff0053ed971c3b728b48df8
Nalen Srin <161912473+NalenSrin123@users.noreply.github.com>
2024-12-25 18:46:18 +0700

    Delete Product directory

--- Product/connection.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $con=new mysqli('localhost','root','','db_stock');
-    // if($con){
-    //     echo 'success';
-    // }else{
-    //     echo 'error';
-    // }
-?>
--- Product/index.php
+++
@@ -1,8 +0,0 @@
-<?php
- session_start();
- if(empty($_SESSION['name'])){
-    header('location: login.php');
- }else{
-    header('location: dashboard.php');
- }   
-?>
